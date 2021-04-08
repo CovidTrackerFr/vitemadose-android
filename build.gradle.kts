fbc51ba6137903ab46d60e060938513a98e6bcbd
Michel Gauzins <michel.gauzins@gmail.com>
2021-04-08 17:18:02 +0200

    Initial setup Firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.jvm").version(Versions.kotlin).apply(false)
     id("org.jetbrains.kotlin.android").version(Versions.kotlin).apply(false)
     id("org.jetbrains.kotlin.android.extensions").version(Versions.kotlin).apply(false)
+    id("com.google.gms.google-services").version(Versions.googleServicesPlugin).apply(false)
+    id("com.google.firebase.crashlytics").version(Versions.firebaseCrashlytics).apply(false)
+    id("com.google.firebase.appdistribution").version(Versions.firebaseAppDistribution).apply(false)
 }
 
 allprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,15 @@ pluginManagement {
             if (requested.id.id == "com.android.library") {
                 useModule("com.android.tools.build:gradle:${requested.version}")
             }
+            if (requested.id.id == "com.google.firebase.crashlytics") {
+                useModule("com.google.firebase:firebase-crashlytics-gradle:${requested.version}")
+            }
+            if (requested.id.id == "com.google.firebase.appdistribution") {
+                useModule("com.google.firebase:firebase-appdistribution-gradle:${requested.version}")
+            }
+            if (requested.id.id == "com.google.gms.google-services") {
+                useModule("com.google.gms:google-services:${requested.version}")
+            }
         }
     }
 }
