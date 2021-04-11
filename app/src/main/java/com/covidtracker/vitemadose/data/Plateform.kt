851d3157917b28faee7638b21da2c93d2627973d
Michel Gauzins <michel.gauzins@gmail.com>
2021-04-11 19:08:44 +0200

    Add pandalab

--- app/src/main/java/com/covidtracker/vitemadose/data/Plateform.kt
+++ app/src/main/java/com/covidtracker/vitemadose/data/Plateform.kt
@@ -7,7 +7,8 @@ enum class Plateform(val id: String, val label: String, @DrawableRes val logo: I
     DOCTOLIB("Doctolib", "Doctolib.fr", R.drawable.logo_doctolib),
     KELDOC("Keldoc", "Keldoc.com", R.drawable.logo_keldoc),
     MAIIA("Maiia", "Maiia.com", R.drawable.logo_maiia),
-    ORDOCLIC("Ordoclic", "Ordoclic.fr", R.drawable.logo_ordoclic);
+    ORDOCLIC("Ordoclic", "Ordoclic.fr", R.drawable.logo_ordoclic),
+    PANDALAB("Pandalab", "Pandalab.fr", R.drawable.logo_pandalab);
 
     companion object {
         fun fromId(id: String): Plateform? = values().firstOrNull { it.id == id }
