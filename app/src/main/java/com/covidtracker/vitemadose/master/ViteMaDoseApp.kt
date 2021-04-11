3b7041991c1c9d83b357ccd1bdbedf6fc5bf70ac
Michel Gauzins <michel.gauzins@gmail.com>
2021-04-11 21:19:09 +0200

    Log level

--- app/src/main/java/com/covidtracker/vitemadose/master/ViteMaDoseApp.kt
+++ app/src/main/java/com/covidtracker/vitemadose/master/ViteMaDoseApp.kt
@@ -1,12 +1,14 @@
 package com.covidtracker.vitemadose.master
 
 import android.app.Application
+import timber.log.Timber
 
 class ViteMaDoseApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+        Timber.plant(Timber.DebugTree());
     }
 
     companion object {
