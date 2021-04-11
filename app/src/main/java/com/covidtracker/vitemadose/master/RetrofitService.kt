24784f08928dc40f0a0cc2cc22d90fa198e036df
Michel Gauzins <michel.gauzins@gmail.com>
2021-04-11 17:30:25 +0200

    AboutScreen

--- app/src/main/java/com/covidtracker/vitemadose/master/RetrofitService.kt
+++ app/src/main/java/com/covidtracker/vitemadose/master/RetrofitService.kt
@@ -2,8 +2,10 @@ package com.covidtracker.vitemadose.master
 
 import com.covidtracker.vitemadose.data.CenterResponse
 import com.covidtracker.vitemadose.data.Department
+import com.covidtracker.vitemadose.data.StatsResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Url
 
 interface RetrofitService {
     @GET("CovidTrackerFr/vitemadose/data-auto/data/output/departements.json")
@@ -12,4 +14,6 @@ interface RetrofitService {
     @GET("CovidTrackerFr/vitemadose/data-auto/data/output/{code}.json")
     suspend fun getCenters(@Path("code") code: String): CenterResponse
 
+    @GET
+    suspend fun getStats(@Url url: String): StatsResponse
 }
