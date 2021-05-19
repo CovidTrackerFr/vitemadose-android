ca2a4e23f79d0faa24eb03d0dda5725652a26200
Michel Gauzins <michel.gauzins@gmail.com>
2021-05-19 23:09:33 +0200

    cleanup

--- app/src/main/java/com/cvtracker/vmd/onboarding/ChronodoseOnboardingActivity.kt
+++ app/src/main/java/com/cvtracker/vmd/onboarding/ChronodoseOnboardingActivity.kt
@@ -5,16 +5,11 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.cvtracker.vmd.R
-import com.cvtracker.vmd.custom.CenterAdapter
-import com.cvtracker.vmd.data.DisplayItem
 import com.cvtracker.vmd.extensions.colorAttr
 import com.cvtracker.vmd.extensions.hide
 import com.cvtracker.vmd.master.PrefHelper
-import kotlinx.android.synthetic.main.activity_about.*
 import kotlinx.android.synthetic.main.activity_about.toolbar
 import kotlinx.android.synthetic.main.activity_chronodose_onboarding.*
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class ChronodoseOnboardingActivity : AppCompatActivity() {
 
@@ -48,28 +43,5 @@ class ChronodoseOnboardingActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-
-    companion object {
-        private val fakeCenter = DisplayItem.Center(
-                department = "35",
-                name = "Centre de vaccination COVID",
-                url = "http",
-                platform = "Doctolib",
-                metadata = DisplayItem.Center.Metadata(
-                        address = "1 Rue Georges Clemenceau, 35400 Saint-Malo",
-                        businessHours = null,
-                        phoneNumber = null
-                ),
-                location = null,
-                nextSlot = Date(Date().time + 12 * 60 * 60 * 1000),
-                appointmentCount = 30,
-                type = "",
-                id = "",
-                vaccineType = listOf("Pfizer-BioNTech"),
-                schedules = listOf(DisplayItem.Center.Schedule("chronodose", 4)),
-                appointmentByPhoneOnly = false
-        )
     }
 }
