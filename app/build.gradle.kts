cc3a8f1f3a5a08484e79e0a3a0d11331559c7535
Michel Gauzins <michel.gauzins@gmail.com>
2021-05-19 23:30:58 +0200

    Version 1.3.3(19) Ready to release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.cvtracker.vmd2"
         setMinSdkVersion(Versions.minSdkVersion)
         setTargetSdkVersion(Versions.targetSdkVersion)
-        versionCode = 18
-        versionName = "1.3.2"
+        versionCode = 19
+        versionName = "1.3.3"
     }
 
     compileOptions {
