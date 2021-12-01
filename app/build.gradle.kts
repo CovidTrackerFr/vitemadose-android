7bb33276b47cf681b680615e0157bdc67eede807
m.gauzins <michel.gauzins@gmail.com>
2021-12-01 19:45:25 +0100

    Version 1.4.1 / Ready to release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.cvtracker.vmd2"
         minSdk = Versions.minSdkVersion
         targetSdk = Versions.targetSdkVersion
-        versionCode = 23
-        versionName = "1.4.0"
+        versionCode = 24
+        versionName = "1.4.1"
     }
 
     compileOptions {
