587c8d2d8a7608e0bf514fda218ffe7be98a626f
m.gauzins <michel.gauzins@gmail.com>
2021-12-22 18:00:24 +0100

    Kid shot feature

--- app/src/main/java/com/cvtracker/vmd/master/TagType.kt
+++ app/src/main/java/com/cvtracker/vmd/master/TagType.kt
@@ -5,5 +5,6 @@ import com.cvtracker.vmd.R
 
 enum class TagType(val key: String, @StringRes val titleRes: Int) {
     FIRST_SHOT( "first_or_second_dose", R.string.tag_type_first_shot),
-    THIRD_SHOT("third_dose", R.string.tag_type_third_shot);
+    THIRD_SHOT("third_dose", R.string.tag_type_third_shot),
+    KID_SHOT( "kids_first_dose", R.string.tag_type_kid),;
 }
